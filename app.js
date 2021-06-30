76b88031743a816d32911c4af3d21c0b07a66b76
Felipe Luis Mendes <felipe.mendes@lexisnexisrisk.com>
2021-06-29 23:17:29 -0300

    fix tests and docker publish

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.get('/', (req, res) => res.json({ message: 'Funcionando!' }))
 app.get('/valorMetroQuadrado', metroQuadrado.buscarValorMetroQuadrado)
 app.post('/calcularValorMetroQuadrado', utils.validaContentTypeBody, utils.validaCampos, (req, res) => metroQuadrado.calcularValorMetroQuadrado(req, res, axiosInstance))
 
-app.listen(port, '0.0.0.0')
+if (require.main === module) {
+	app.listen(port, '0.0.0.0')
+}
 
 module.exports = app
